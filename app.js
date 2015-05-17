00589b70c62198cc24f3e4a9828a8795118c4ea7
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-17 11:11:33 -0400

    update form to include text box to add a link

--- app.js
+++ app.js
@@ -10,8 +10,13 @@ angular.module('flapperNews', [])
     ];
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({ title: $scope.title, upvotes: 0 } );
-      $scope.title= '';
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
     };
 
     $scope.incrementUpvotes = function(post) {
