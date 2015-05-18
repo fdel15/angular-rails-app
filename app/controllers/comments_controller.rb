fce8065aa470dbffdc66c300bb9d0892b98e8bde
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-18 01:45:57 -0400

    Controllers: create create action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
 
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.create(comment_params)
+    respond_with post, comment
+  end
+
   private
 
   def comment_params
