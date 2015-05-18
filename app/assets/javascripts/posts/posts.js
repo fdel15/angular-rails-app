15eb5a3cf27bb1738c4b5f65d5b8386f2dc04c43
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-18 13:06:04 -0400

    JS: add getAll function to posts service

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -2,5 +2,10 @@ app.factory('posts', ['$http', function($http){
     var o = {
       posts: []
     };
+    o.getAll = function() {
+      return $http.get('/posts.json').success(function(data){
+        angular.copy(data, o.posts);
+      });
+    };
     return o;
   }])
