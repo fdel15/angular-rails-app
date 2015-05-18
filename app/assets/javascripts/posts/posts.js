016bc1a7a80da23a96a42002b0105c5e89070e06
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-18 13:19:28 -0400

    JS: add create function to posts service

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -7,5 +7,10 @@ app.factory('posts', ['$http', function($http){
         angular.copy(data, o.posts);
       });
     };
+    o.create = function(new_post) {
+      return $http.post('/posts.json', new_post).success(function(data){
+        o.posts.push(data);
+      })
+    }
     return o;
   }])
