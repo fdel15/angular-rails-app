b1d6c2c7273731e7f9bcf95e60cf0fd17d4187a5
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-21 02:06:33 -0400

    create number_of_votes metod for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def as_json(options= {})
-    super(options.merge(include: [:user, :votes, comments: { include: [:user, :votes]}]))
+    super(options.merge(include: [:user, :votes, comments: { include: [:user, :votes]}], methods: :number_of_votes))
+  end
+
+  def number_of_votes
+    total_votes = votes.count
+    upvotes = votes.select{|vote| vote.type == "up"}.count
+    downvotes = total_votes - upvotes
+    return upvotes - downvotes
   end
 end
